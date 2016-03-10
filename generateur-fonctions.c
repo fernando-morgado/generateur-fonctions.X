c44715c0250c33a24ec7c7a73c05550f1735a67c
jean-michel-gonet <jmgonet@yahoo.com>
2016-03-10 20:55:29 +0100

    Version de base

--- generateur-fonctions.c
+++ generateur-fonctions.c
@@ -14,20 +14,60 @@
 #pragma config LVP = OFF       // Single Supply Enable bits off.
 
 /**
+ * Fonction qui transmet un caractère à la EUSART.
+ * Il s'agit de l'implémentation d'une fonction système qui est
+ * appelée par <code>printf</code>.
+ * Cette implémentation envoie le caractère à la UART. Si un terminal
+ * est connecté aux sorties RX / TX, il affichera du texte.
+ * @param data Le code ASCII du caractère à afficher.
+*/
+void putch(char data) {
+    while( ! TX1IF);
+    TXREG1 = data;
+}
+
+/**
+ * Initialise la sortie 1 de la EUSART.
+ * Si le UP est à 1MHz, le Virtual Terminal dans Proteus
+ * doit être configuré comme suit:
+ * - 1200 bauds.
+ * - Transmission 8 bits.
+ * - Bit de stop activé.
+ */
+void initialiseEUSART() {
+    // Pour une fréquence de 1MHz, ceci donne 1200 bauds:
+    SPBRG = 12;
+    SPBRGH = 0;
+    // Configure RC6 et RC7 comme entrées digitales, pour que
+    // la EUSART puisse en prendre le contrôle:
+    TRISCbits.RC6 = 1;
+    TRISCbits.RC7 = 1;
+    
+    // Configure la EUSART:
+    // (BRGH et BRG16 sont à leur valeurs par défaut)
+    // (TX9 est à sa valeur par défaut)
+    RCSTAbits.SPEN = 1;  // Active la EUSART.
+    TXSTAbits.SYNC = 0;  // Mode asynchrone.
+    TXSTAbits.TXEN = 1;  // Active l'émetteur.
+}
+
+char generateur() {
+    char sinus[] = {15,19,23,27,29,30,29,27,23,19,15,10,6,2,0,0,0,2,6,10,14};
+    return 15;
+}
+
+
+/**
  * Point d'entrée du programme.
  */
 void main(void) {
-    char sinus[] = {15,19,23,27,29,30,29,27,23,19,15,10,6,2,0,0,0,2,6,10,14};
     unsigned char n;
     
-    ANSELA = 0;
-    ANSELB = 0;
-    ANSELC = 0;
-    TRISA = 0;
+    initialiseEUSART();
 
-    while(1) {
-        for (n = 0; n < 20; n++) {
-            PORTA = sinus[n];
-        }
+    if (generateur() == 15) {
+        printf("Ok 1");
     }
+    
+    while(1);
 }
